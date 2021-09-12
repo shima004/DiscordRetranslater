6098a880d45dc42692b20042094c503e6e54e418
shima004 <yellow030408@gmail.com>
2021-09-12 15:48:51 +0900

    [add] youtube

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
-from discord.ext import commands
+import discord
 import requests
+import asyncio
 import csv
 import os
 import youtube_dl
@@ -55,7 +56,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.url = data.get('url')
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
+    async def from_url(cls, url, volume, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
@@ -64,18 +65,18 @@ class YTDLSource(discord.PCMVolumeTransformer):
             data = data['entries'][0]
 
         filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data, volume=volume)
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
+volume = 0.5
 
+# @bot.event
+# async def on_ready():
+#   print("bot started")
 
-@bot.event
-async def on_ready():
-  print("bot started")
-
-@bot.event
-async def on_message(message):
-  global translate_langs
+@client.event
+async def on_message(message: discord.Message):
+  global translate_langs, volume
   if message.author.bot:
     return
   elif message.content.startswith("http"):
@@ -107,7 +108,14 @@ async def on_message(message):
       "/bot : 現在のボットの設定\n" +
       "/help : コマンドについての説明を表示する\n" +
       "/langlist : 使うことのできる言語コードを表示する\n"+
-      "/rand [int: num] : ランダムで言語を設定する 0 < num < 10 ex.) /rand 3"
+      "/rand [int: num] : ランダムで言語を設定する 0 < num < 10 ex.) /rand 3 \n"
+      "/join : 現在いるチャンネルに入る\n" +
+      "/leave : 現在いるチャンネルから出る\n" +
+      "/play [url: string]: YoutubeのURLを送ると再生する\n" +
+      "/pause : 動画を一時停止する\n" +
+      "/stop : 動画を終了する\n" +
+      "/resume : 動画を再開する\n" +
+      "/volume [int: num]: 0 < num < 100"
     )
   elif message.content.startswith("/t"): # /translate
     msg = "".join(message.content.split(" ")[1:])
@@ -139,12 +147,46 @@ async def on_message(message):
   elif message.content.startswith("/play"):
     if message.guild.voice_client is None:
       await message.channel.send("接続していません。")
+    elif message.guild.voice_client.is_playing():
+      await message.channel.send("再生中です。")
     else:
       url = message.content.split(" ")[1]
-      player = await YTDLSource.from_url(url, loop=bot.loop)
-      await message.guild.voice_client.play(player)
+      player = await YTDLSource.from_url(url, volume, loop=client.loop)
+      message.guild.voice_client.play(player)
+      await client.change_presence(activity=discord.Game(name="{}".format(player.title)))
       await message.channel.send('{} を再生します。'.format(player.title))
+  elif message.content.startswith("/stop"):
+    if message.guild.voice_client is None:
+        await message.channel.send("接続していません。")
+    elif not message.guild.voice_client.is_playing():
+        await message.channel.send("再生していません。")
+    else:
+      message.guild.voice_client.stop()
+      await message.channel.send("ストップしました。")
+  elif message.content.startswith("/pause"):
+    if message.guild.voice_client is None:
+        await message.channel.send("接続していません。")
+    elif not message.guild.voice_client.is_playing():
+        await message.channel.send("再生していません。")
+    else:
+      message.guild.voice_client.pause()
+      await message.channel.send("一時停止しました。")
+  elif message.content.startswith("/resume"):
+    if message.guild.voice_client is None:
+        await message.channel.send("接続していません。")
+    elif not message.guild.voice_client.is_playing():
+        await message.channel.send("再生していません。")
+    else:
+      message.guild.voice_client.resume()
+      await message.channel.send("再開しました。")
+  elif message.content.startswith("/volume"):
+    volume_input = int(message.content.split(" ")[1])
+    if 0 <= volume_input <= 100:
+      volume = volume_input / 100
+      await message.channel.send("音量を {}% に設定しました。".format(volume_input))
+    else:
+      await message.channel.send("音量を 0 ~ 100 まで設定してください。")
   else:
     return
 
-bot.run(TOKEN)
+client.run(TOKEN)
